452f0449f02c50795b6515b7f3f7c42b72212eda
Lawrence Emeka <mrabarah@gmail.com>
2024-04-15 18:00:50 +0100

    add styles and logic to flash message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
     <title>Laravel 10 Task List App</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <script src="//unpkg.com/alpinejs" defer></script>
 
     {{--blade-formater-disable --}}
     <style type="text/tailwindcss">
@@ -32,10 +33,24 @@
 
 <body class="container mx-auto mt-10 mb-10 max-w-lg">
     <h1 class="text-2xl mb-4">@yield('title')</h1>
-    <div>
-        @if (session()->has('success'))
+    <div x-data="{ flash: true }">
+         @if (session()->has('success'))
             <div>{{ session('success') }}</div>
-        @endif
+            
+             
+            <div x-show="flash" class="relative mb-10 rounded border boder-green-400 bg-green-100 px-4 py-3 text-lg text-green-700" role="alert">
+                    <strong class="font-bold">success!</strong>
+                    <div>{{ session('success') }}</div>
+
+                    <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                stroke-width="1.5" @click="flash = false"
+                                stroke="currentColor" class="h-6 w-6 cursor-pointer">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </span>
+            </div>
+         @endif
        @yield('content') 
     </div>
 </body>
